648293e80f26d04d62090c9c05f1fd4dd7e60638
shintayo14 <shintayoshika@gmail.com>
2022-07-04 09:32:57 +0900

    change data-type of columns except delivery-date

--- 2021auTBvis.py
+++ 2021auTBvis.py
@@ -8,4 +8,18 @@ from modules import dataset_function as ds_func
 
 df = ds_func.import_tb_dataset("SIR51015.csv")
 
+# st.write(df.head(3))
+
+df = df[~df['受渡日'].isnull()]
+df['案件ID'] = df['案件ID'].apply(lambda x : str(x) if type(x) != str else x)
+df['購入・予約管理ID'] = df['購入・予約管理ID'].apply(lambda x: str(x) if type(x) != str else x)
+df['案件ID_案件名'] = df['案件ID'].str.cat(df['案件名'], sep='_')
+
+
+df['受渡日'] = df['受渡日'].apply(lambda x: str(x) if type(x) != str else x)
+
+from pandas.api.types import infer_dtype
+st.write(df.apply(infer_dtype))
+
 st.write(df.head(3))
+
